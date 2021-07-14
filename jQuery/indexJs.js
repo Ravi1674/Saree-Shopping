6f8c0c46dd725abb0ebec4baffec7885b3bbc32e
Ravi Chavda <chavdaraviraj364@gmail.com>
2021-07-14 14:46:09 +0530

    first commit

---
+++ jQuery/indexJs.js
@@ -0,0 +1,40 @@
+$(function(){
+    //For recent product
+    $('#b1').mouseover(function(){ $('#i1').show(); });
+    $('#b1').mouseout(function(){ $('#i1').hide(); });
+
+    $('#b2').mouseover(function(){ $('#i2').show(); });
+    $('#b2').mouseout(function(){ $('#i2').hide(); });
+
+    $('#b3').mouseover(function(){ $('#i3').show(); });
+    $('#b3').mouseout(function(){ $('#i3').hide(); });
+
+    $('#b4').mouseover(function(){ $('#i4').show(); });
+    $('#b4').mouseout(function(){ $('#i4').hide(); });
+
+    $('#b5').mouseover(function(){ $('#i5').show(); });
+    $('#b5').mouseout(function(){ $('#i5').hide(); });
+
+    $('#b6').mouseover(function(){ $('#i6').show(); });
+    $('#b6').mouseout(function(){ $('#i6').hide(); });
+
+    //Popular Product
+    $('#p1').mouseover(function(){ $('#pi1').show(); });
+    $('#p1').mouseout(function(){ $('#pi1').hide(); });
+
+    $('#p2').mouseover(function(){ $('#pi2').show(); });
+    $('#p2').mouseout(function(){ $('#pi2').hide(); });
+
+    $('#p3').mouseover(function(){ $('#pi3').show(); });
+    $('#p3').mouseout(function(){ $('#pi3').hide(); });
+
+    //Trending Product
+    $('#t1').mouseover(function(){ $('#ti1').show(); });
+    $('#t1').mouseout(function(){ $('#ti1').hide(); });
+
+    $('#t2').mouseover(function(){ $('#ti2').show(); });
+    $('#t2').mouseout(function(){ $('#ti2').hide(); });
+
+    $('#t3').mouseover(function(){ $('#ti3').show(); });
+    $('#t3').mouseout(function(){ $('#ti3').hide(); });
+});
